27d4531ddc78c9c21b6edefaf7fe8c48c2a510ba
Kunal Jaykam <50500283+kunaljaykam@users.noreply.github.com>
2022-05-12 17:06:07 +0000

    update github dev container

--- .devcontainer/github-codespaces-scripts.sh
+++ .devcontainer/github-codespaces-scripts.sh
@@ -13,7 +13,9 @@ sudo mysql -uroot -Bse  "create database sakaidb  default character set utf8; cr
 "
 
 # create sakai.properties in workspace directory
-cd ../..
+cd
+
+cd /workspaces
 
 wget https://github.com/kunaljaykam/kj-sakai/raw/main/sakai-github-codespaces-template/sakai.properties.zip
 
