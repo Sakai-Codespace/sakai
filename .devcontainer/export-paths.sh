f5ff0374592fd861fd5849e0f75bf02457a2e7f0
Kunal Jaykam <50500283+kunaljaykam@users.noreply.github.com>
2022-05-13 10:13:45 +0000

    update github dev container

--- .devcontainer/export-paths.sh
+++ .devcontainer/export-paths.sh
@@ -1,7 +1,9 @@
-export JAVA_HOME=/opt/java/11.0.14
-export PATH=$PATH:$JAVA_HOME/bin
 
-export MAVEN_HOME=/opt/maven/3.6.3
-export PATH=$PATH:$MAVEN_HOME/bin
 
-exec
+# save these paths for all terminals
+echo "export JAVA_HOME=/opt/java/11.0.14" >> ~/.bashrc
+echo "export PATH=$PATH:$JAVA_HOME/bin" >> ~/.bashrc
+echo "export MAVEN_HOME=/opt/maven/3.6.3" >> ~/.bashrc
+echo "export PATH=$PATH:$MAVEN_HOME/bin" >> ~/.bashrc
+
+exec bash
